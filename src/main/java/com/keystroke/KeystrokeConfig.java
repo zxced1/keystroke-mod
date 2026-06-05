5518e3abd7ca188d6f4f21fa9775deaeb85dc494
Kacper Duraj <157953967+zxced1@users.noreply.github.com>
2026-06-05 14:21:52 +0200

    Add config file for keystroke display

---
+++ src/main/java/com/keystroke/KeystrokeConfig.java
@@ -0,0 +1,18 @@
+package com.keystroke;
+
+public class KeystrokeConfig {
+    // Position settings (you can change these!)
+    public static int posX = 10;
+    public static int posY = 10;
+    
+    // Key size
+    public static final int KEY_SIZE = 30;
+    public static final int KEY_SPACING = 3;
+    
+    // Colors
+    public static final int COLOR_INACTIVE = 0xFF1a1a1a; // Dark gray/black
+    public static final int COLOR_ACTIVE = 0xFF00FF00;   // Green
+    public static final int COLOR_BORDER = 0xFFFFFFFF;   // White
+    public static final int COLOR_TEXT_ACTIVE = 0xFF000000; // Black text when active
+    public static final int COLOR_TEXT_INACTIVE = 0xFFFFFFFF; // White text when inactive
+}
